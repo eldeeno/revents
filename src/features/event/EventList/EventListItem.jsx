2eb588808867af99ac1a6a7cfd565784c21da506
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-07-15 15:46:54 +0100

    Added state management

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -18,7 +18,7 @@ class EventListItem extends Component {
                     <Item.Content>
                       <Item.Header as="a">{event.title}</Item.Header>
                       <Item.Description>
-                        Hosted by <a>{event.hostBy}</a>
+                        Hosted by <a>{event.hostedBy}</a>
                       </Item.Description>
                     </Item.Content>
                   </Item>
